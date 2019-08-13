70b7a41ddfe417613017ac6e41ffb596babebdd1
jiwen xin <kevinxin@scripps.edu>
2019-08-13 11:24:56 -0700

    add print

--- handlers/hint.py
+++ handlers/hint.py
@@ -8,6 +8,7 @@ ht = Hint()
 class HintHandler(BaseHandler):
     def get(self):
         _input = self.get_query_argument('q', None)
+        print('input is {}'.format(_input))
         if _input:
             result = ht.query(_input)
             self.set_status(200)
